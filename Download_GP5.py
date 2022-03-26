a31aa983e0602fd63de98d9a0535364e53fec124
Rob Alley <rob.alley@gmail.com>
2022-03-26 14:53:19 +1300

    Download each sequence into a seperate directory

--- Download_GP5.py
+++ Download_GP5.py
@@ -2,14 +2,13 @@
 import sys
 import os
 import subprocess
-import re
 import json
 from goprocam import GoProCamera, constants
 
 # Currently hard coded for my Hero 5
 gopro_wifi="GP54508924"
 gopro_bt="D8:59:3A:63:0A:8F"
-local_dir="Timelapse_GP5"
+camera="_GP5"
 
 # Save SSID for currently connected WiFi
 results=subprocess.run(["iwgetid","-r"], capture_output=True, text=True)
@@ -41,24 +40,39 @@ except:
 else:
     print("Connected to GoPro")
 
-    if not os.path.exists(local_dir):
-        os.makedirs(local_dir)
-    os.chdir(local_dir)
+    # Place all images beneath a camera specific directory
+    if not os.path.exists(camera):
+        os.makedirs(camera)
+    os.chdir(camera)
 
     # TODO: Check this works for all sequences, accross directories etc.
+    sequences=[]
     media = json.loads(gpCam.listMedia())
     for directory in media["media"]:
         dirname  = directory["d"]
         for mediaFile in directory["fs"]:
             filename = mediaFile["n"];
+
+            # If file has a 'b' (begin?) entry, it represents a sequence (timelapse or burst)
             if 'b' in mediaFile:
                 base=filename[:4]
+                dirName=f"{base}_{camera}"
+
+                # Place each sequence in it's own directory
+                if not os.path.exists(dirName):
+                    os.makedirs(dirName)
+                os.chdir(dirName)
+
                 start=int(mediaFile["b"])
                 end=int(mediaFile["l"])
                 for i in range(start,end+1):
-                    print(f"Download then delete {dirname}/{base}{i:04d}.JPG")
-                    gpCam.downloadMedia(dirname,f"{base}{i:04d}.JPG")
-                    gpCam.deleteFile(dirname, f"{base}{i:04d}.JPG")
+                    image=f"{base}{i:04d}.JPG"
+                    #print(f"Download then delete {dirname}/{image}")
+                    if i==start:
+                        sequences.append((dirName, image))
+                    gpCam.downloadMedia(dirname, image)
+                    gpCam.deleteFile(dirname,  image)
+                os.chdir("..")
             else:
                 print(f"Ignoring non timelapse file {mediaFile['n']}")
 
@@ -66,6 +80,6 @@ else:
     print("Turning off GoPro...")
     gpCam.power_off()
 
-# FIXME: Why does reconnecting to UnifiAP throw an error but others work ok?
+# FIXME: ssid as reported by iwgetid is not always the same as name/id used by nmcli so this can fail
 print(f"Re-connecting to previous WiFi network '{ssid}'...")
 subprocess.run(["nmcli","c","up", "id", ssid])
