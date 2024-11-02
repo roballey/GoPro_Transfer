215968f4fa5b65c85614351ca9b127be482083b0
Rob Alley <rob.alley@gmail.com>
2024-11-03 07:53:59 +1300

    (WIP) Support MTP transfers

--- Download_GoPro.py
+++ Download_GoPro.py
@@ -79,6 +79,7 @@ for i in config['cameras']:
     if i['camera'] == sys.argv[1]:
         gopro_bt = i['bt']
         gopro_wifi = i['wifi']
+        gopro_mtp = i['mtp']
         camera = i['camera']
         #print(f"{camera} {gopro_bt} {gopro_wifi}")
 
@@ -86,109 +87,120 @@ if camera is None:
     print(f"No entry for camera {sys.argv[1]} in '{configFile}'")
     sys.exit(1)
 
-# Save SSID for currently connected WiFi
-results=subprocess.run(["iwgetid","-r"], capture_output=True, text=True)
-ssid=results.stdout.rstrip("\n")
+if os.path.exists(gopro_mtp):
+    print(f"{camera} connected via USB/MTP...")
 
-# Try connecting to GoPro via Bluetooth and turning on Wifi,
-print(f"Establishing BlueTooth connection to GoPro '{camera}'...")
-print("-------------------------------------------------------\n")
-bt_connected=False
-bt_tried=0
-
-while ( (not bt_connected) and (bt_tried < 2)):
-    try:
-      bt_tried = bt_tried+1
-      print(f"  Connection attempt {bt_tried}")
-      asyncio.run(ble.run(gopro_bt, "wifi on"))
-
-    # If BT connection fails, prompt user to turn GoPro on and try again...
-    except:
-        if (bt_tried < 2):
-          print(f"  Unable to connect via Bluetooth")
-          input(f"    Ensure remote is not connected.\n    Power on GoPro '{camera}' and press <ENTER>: ")
-
-    else:
-        bt_connected=True
-
-if (not bt_connected):
-  print(f"Unable to connect to '{camera}' via Bluetooth")
-  sys.exit(1)
-
-# Connect computer to GoPro WiFi
-print(f"Connecting to GoPro '{camera}' Wifi network '{gopro_wifi}'...")
-print("-------------------------------------------------------\n")
-results=subprocess.run(["nmcli","c","up", "id", gopro_wifi])
-print(f"Status = {results.returncode}")
-
-try:
-    gpCam = GoProCamera.GoPro()
-
-except:
-    print(f"Unable to connect to GoPro: {sys.exc_info()[0]}");
+    print("FIXME: Transfer files.")
+    # Can spawn command and use "gio copy <SRC> <DEST>" to copy files from MTP mount
+    quit()
 
 else:
-    # Report camera overview
-    gpCam.overview()
+    print("Camera not connected via USB/MTP, trying Bluetooth/WiFi")
 
-    # Place all files beneath a camera specific directory
-    now = datetime.now().strftime("%Y-%m-%d") 
-    cameraDir=f"{now}_{camera}"
-    if not os.path.exists(cameraDir):
-        os.makedirs(cameraDir)
-    os.chdir(cameraDir)
-    print(f"GoPro files will be downloaded to {cameraDir}")
+    # Save SSID for currently connected WiFi
+    results=subprocess.run(["iwgetid","-r"], capture_output=True, text=True)
+    ssid=results.stdout.rstrip("\n")
+
+    # Try connecting to GoPro via Bluetooth and turning on Wifi,
+    print(f"Establishing BlueTooth connection to GoPro '{camera}'...")
     print("-------------------------------------------------------\n")
+    bt_connected=False
+    bt_tried=0
 
-    media = json.loads(gpCam.listMedia())
+    while ( (not bt_connected) and (bt_tried < 2)):
+        try:
+          bt_tried = bt_tried+1
+          print(f"  Connection attempt {bt_tried}")
+          asyncio.run(ble.run(gopro_bt, "wifi on"))
 
-    for directory in media["media"]:
-        dirname  = directory["d"]
-        for mediaFile in directory["fs"]:
-            filename = mediaFile["n"];
+        # If BT connection fails, prompt user to turn GoPro on and try again...
+        except:
+            if (bt_tried < 2):
+              print(f"  Unable to connect via Bluetooth")
+              input(f"    Ensure remote is not connected.\n    Power on GoPro '{camera}' and press <ENTER>: ")
 
-            # If file has a 'b' (begin?) entry, it represents a sequence (timelapse or burst)
-            if 'b' in mediaFile:
-                base=filename[:4]
-                dirName=f"{base}_{camera}"
+        else:
+            bt_connected=True
 
-                # Place each sequence in it's own directory
-                if not os.path.exists(dirName):
-                    os.makedirs(dirName)
-                os.chdir(dirName)
+    if (not bt_connected):
+      print(f"Unable to connect to '{camera}' via Bluetooth")
+      sys.exit(1)
 
-                start=int(mediaFile["b"])
-                end=int(mediaFile["l"])
-                print(f"---Download sequence of {end-start} images")
-                for i in range(start,end+1):
-                    image=f"{base}{i:04d}.JPG"
-                    if i==start:
-                        sequences.append((dirName, image))
-                    print(f"   ---Download image {i-start}/{end-start} ",end=" ")
-                    gpCam.downloadMedia(dirname,image)
-                    gpCam.deleteFile(dirname, image)
-                os.chdir("..")
-            else:
-                # Place non-timelapse files in their own directory
-                dirName=f"NonTimeLapse_{camera}"
-                if not os.path.exists(dirName):
-                    os.makedirs(dirName)
-                os.chdir(dirName)
-                image=mediaFile['n']
-                print(f"---Download non-timelapse file ",end=" ")
-                gpCam.downloadMedia(dirname,filename)
-                gpCam.deleteFile(dirname, filename)
-                os.chdir("..")
-
-    print("Turning off GoPro...")
+    # Connect computer to GoPro WiFi
+    print(f"Connecting to GoPro '{camera}' Wifi network '{gopro_wifi}'...")
     print("-------------------------------------------------------\n")
-    gpCam.power_off()
+    results=subprocess.run(["nmcli","c","up", "id", gopro_wifi])
+    print(f"Status = {results.returncode}")
 
-# FIXME: ssid as reported by iwgetid is not always the same as name/id used by nmcli (sometimes it has "Auto" pre-pended)
-print(f"Re-connecting to previous WiFi network '{ssid}'...")
-print("-------------------------------------------------------\n")
-subprocess.run(["nmcli","c","up", "id", ssid])
+    try:
+        gpCam = GoProCamera.GoPro()
 
-time.sleep(2)  # Delay to ensure network reconnection is complete
+    except:
+        print(f"Unable to connect to GoPro: {sys.exc_info()[0]}");
 
+    else:
+        # Report camera overview
+        gpCam.overview()
+
+        # Place all files beneath a camera specific directory
+        now = datetime.now().strftime("%Y-%m-%d") 
+        cameraDir=f"{now}_{camera}"
+        if not os.path.exists(cameraDir):
+            os.makedirs(cameraDir)
+        os.chdir(cameraDir)
+        print(f"GoPro files will be downloaded to {cameraDir}")
+        print("-------------------------------------------------------\n")
+
+        media = json.loads(gpCam.listMedia())
+
+        for directory in media["media"]:
+            dirname  = directory["d"]
+            for mediaFile in directory["fs"]:
+                filename = mediaFile["n"];
+
+                # If file has a 'b' (begin?) entry, it represents a sequence (timelapse or burst)
+                if 'b' in mediaFile:
+                    base=filename[:4]
+                    dirName=f"{base}_{camera}"
+
+                    # Place each sequence in it's own directory
+                    if not os.path.exists(dirName):
+                        os.makedirs(dirName)
+                    os.chdir(dirName)
+
+                    start=int(mediaFile["b"])
+                    end=int(mediaFile["l"])
+                    print(f"---Download sequence of {end-start} images")
+                    for i in range(start,end+1):
+                        image=f"{base}{i:04d}.JPG"
+                        if i==start:
+                            sequences.append((dirName, image))
+                        print(f"   ---Download image {i-start}/{end-start} ",end=" ")
+                        gpCam.downloadMedia(dirname,image)
+                        gpCam.deleteFile(dirname, image)
+                    os.chdir("..")
+                else:
+                    # Place non-timelapse files in their own directory
+                    dirName=f"NonTimeLapse_{camera}"
+                    if not os.path.exists(dirName):
+                        os.makedirs(dirName)
+                    os.chdir(dirName)
+                    image=mediaFile['n']
+                    print(f"---Download non-timelapse file ",end=" ")
+                    gpCam.downloadMedia(dirname,filename)
+                    gpCam.deleteFile(dirname, filename)
+                    os.chdir("..")
+
+        print("Turning off GoPro...")
+        print("-------------------------------------------------------\n")
+        gpCam.power_off()
+
+    # FIXME: ssid as reported by iwgetid is not always the same as name/id used by nmcli (sometimes it has "Auto" pre-pended)
+    print(f"Re-connecting to previous WiFi network '{ssid}'...")
+    print("-------------------------------------------------------\n")
+    subprocess.run(["nmcli","c","up", "id", ssid])
+
+    time.sleep(2)  # Delay to ensure network reconnection is complete
+
+print("Renaming directories...")
 rename_directories(sequences)
