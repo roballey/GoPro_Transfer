113d0971f32cfc91d3c53e9583262b30c4a671ae
Rob Alley <rob.alley@gmail.com>
2026-04-05 16:11:56 +1200

    Add shell scripts to activate venv and run

---
+++ GoProHero10Transfer.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/bash
+# Activate venv and run script to transfer GoPro Hero 10 files...
+source ~/Projects/GoProTransfer/Venv/bin/activate
+python3 ~/Projects/GoProTransfer/Download_GoPro.py Hero10
---
+++ GoProMaxTransfer.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+# Activate venv and run script to transfer GoPro Max files...
+# (Max4 as it's the fourth Max I've owned!)
+source ~/Projects/GoProTransfer/Venv/bin/activate
+python3 ~/Projects/GoProTransfer/Download_GoPro.py Max4
